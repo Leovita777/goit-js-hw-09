27a7df9a16f0aca05138c979023761bb3e7ae532
Leovita777 <141558126+Leovita777@users.noreply.github.com>
2024-01-24 23:17:57 +0100

    'update'

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+import { localStorage } from 'local-storage';
+
+function saveFormState() {
+  const email = document.querySelector("input[name='email']").value;
+  const message = document.querySelector("textarea[name='message']").value;
+
+  const state = { email, message };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}
+
+function loadFormState() {
+  const state = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (state) {
+    document.querySelector("input[name='email']").value = state.email;
+    document.querySelector("textarea[name='message']").value = state.message;
+  }
+}
+
+function clearFormState() {
+  localStorage.removeItem('feedback-form-state');
+
+  document.querySelector("input[name='email']").value = '';
+  document.querySelector("textarea[name='message']").value = '';
+}
+
+document
+  .querySelector('.feedback-form')
+  .addEventListener('input', saveFormState);
+
+loadFormState();
+
+document.querySelector('form').addEventListener('submit', () => {
+  clearFormState();
+
+  console.log({
+    email: document.querySelector("input[name='email']").value,
+    message: document.querySelector("textarea[name='message']").value,
+  });
+});
